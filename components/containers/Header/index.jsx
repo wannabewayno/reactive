c2c5a9013de73566a0c240f38b3a6dadcbed55fb
Wayne <wayne.c.griffiths@gmail.com>
2020-07-12 14:28:04 +0800

    UPDATE package.json: installed React as a dependency

--- components/containers/Header/index.jsx
+++ components/containers/Header/index.jsx
@@ -6,12 +6,12 @@ function Header({ title, links, position = 'relative', height = '50px'}){
 
     const style = { height, position }
 
-return (
-    <header className='Header' style={style}>
-        {title?<h1>{title}</h1>:undefined}
-        {links?<LinkContainer links={links}/>:undefined}
-    </header>
-)
+    return (
+        <header className='Header' style={style}>
+            {title?<h1>{title}</h1>:undefined}
+            {links?<LinkContainer links={links}/>:undefined}
+        </header>
+    )
 }
 
 export default Header;
