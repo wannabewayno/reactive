ad224e3cf81c14ede5bcee516813a5a06cb5c949
Wayne <wayne.c.griffiths@gmail.com>
2020-07-13 21:19:16 +0800

    version change 0.0.3 -> 0.0.4

---
+++ index.js
@@ -0,0 +1,6 @@
+import Dropdown from './src/components/Dropdown';
+
+
+export {
+    Dropdown
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
+// webpack.config.js
 const path = require('path');
 
+
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'reactor.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            },
-        ]
-    }
-}
+  entry: './src/components/Dropdown/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+    ]
+  },
+  externals: {
+    'react': 'commonjs react' 
+  }
+};
