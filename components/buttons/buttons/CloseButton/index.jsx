dad8255e3249bf5b8fb4029b56782b294b5be4d5
Wayne <wayne.c.griffiths@gmail.com>
2020-07-14 10:46:20 +0800

    commenting out CloseButton for testing

--- components/buttons/buttons/CloseButton/index.jsx
+++ components/buttons/buttons/CloseButton/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './style.css';
-import iconSize from '../Button/iconSize';
-import Button from '../Button';
+// import React from 'react';
+// import './style.css';
+// import iconSize from '../Button/iconSize';
+// import Button from '../Button';
 
 
-export default function CloseButton(props){
+// export default function CloseButton(props){
 
-    return (
-        <Button {...props}>
-            <img src="./images/icons/close-icon.png" alt="close button" style={iconSize('small')}/>
-        </Button>
-    )
-}
+//     return (
+//         <Button {...props}>
+//             <img src="./images/icons/close-icon.png" alt="close button" style={iconSize('small')}/>
+//         </Button>
+//     )
+// }
