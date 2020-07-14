45721fcd635f8eb3cb1b8e19a799c275037eeecd
Wayne <wayne.c.griffiths@gmail.com>
2020-07-14 11:03:16 +0800

    removed all components except 'Dropdown'

--- src/components/buttons/Links/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style.css';
-
-export default function Link({ href, display, style }){
-    return <a href={href} className={`link ${style? style:'nude'}`}>{display}</a>
-}
-
