c7624deae7530f5e7b4fdb9fed163194e85f3abb
Wayne <wayne.c.griffiths@gmail.com>
2020-07-15 18:22:53 +0800

    update container for children to have keys

--- src/components/containers/Container/index.jsx
+++ src/components/containers/Container/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import './style.css';
 
 const Container = ({ children, style }) => {
     if(!style) style = {};
-    
+
     return (
         <section className='Container' style={style}>
-            {children}
+            {children.map((child,index) => cloneElement(child,{ key:index }))}
         </section>
     )
 }
