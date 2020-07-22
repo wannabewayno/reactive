dd49e8c726a13f943c26867f16b82c9d97e592eb
Wayne <wayne.c.griffiths@gmail.com>
2020-07-22 12:47:13 +0800

    DELETE console.log(statename_ in useLiftState hook

--- src/hooks/useLiftState/index.jsx
+++ src/hooks/useLiftState/index.jsx
@@ -5,7 +5,7 @@ export default function useLiftState(){
 
     function liftUpState(stateName,state,setState){
         let setStateName = 'set' + stateName.slice(0,1).toUpperCase() + stateName.slice(1);
-        console.log(stateName);
+        
         setLiftedStates({...liftedStates, [stateName]:state, [setStateName]:setState })
     }
 
