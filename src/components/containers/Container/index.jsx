ca33aa55630464a80cf4678eefc2b2bbf8f4b13c
Wayne <wayne.c.griffiths@gmail.com>
2020-07-22 12:45:52 +0800

    DELETE useEffect from Container

--- src/components/containers/Container/index.jsx
+++ src/components/containers/Container/index.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './style.css';
 
 export default function Container({ children, style }) {
 
     style = { margin:'0 auto', textAlign:'center', ...style }
-
-    // need to add media queries in JS an remove css
-    useEffect(() => null,[]);
 
     return (
         <section className='Container' style={style}>
